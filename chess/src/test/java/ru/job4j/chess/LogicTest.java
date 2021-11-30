4aad38daa08448438a2b8512ee7ba22c69daf062
AlenaAgeeva <alenapvageeva@gmail.com>
2021-11-30 10:50:12 -0400

    2. Каркас шахматной доски [#792 #234072]

--- chess/src/test/java/ru/job4j/chess/LogicTest.java
+++ chess/src/test/java/ru/job4j/chess/LogicTest.java
@@ -7,7 +7,8 @@ import ru.job4j.chess.firuges.black.BishopBlack;
 public class LogicTest {
 
     @Test(expected = FigureNotFoundException.class)
-    public void figureNotFound() throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
+    public void figureNotFound()
+            throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
         Logic logic = new Logic();
         logic.add(new BishopBlack(Cell.A1));
         logic.add(new BishopBlack(Cell.B2));
@@ -15,14 +16,16 @@ public class LogicTest {
     }
 
     @Test(expected = ImpossibleMoveException.class)
-    public void impossibleMove() throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
+    public void impossibleMove()
+            throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
         Logic logic = new Logic();
         logic.add(new BishopBlack(Cell.A1));
         logic.move(Cell.A1, Cell.A3);
     }
 
     @Test(expected = OccupiedCellException.class)
-    public void occupied() throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
+    public void occupied()
+            throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
         Logic logic = new Logic();
         logic.add(new BishopBlack(Cell.A1));
         logic.add(new BishopBlack(Cell.B2));
--- chess/src/test/java/ru/job4j/chess/firuges/black/BishopBlackTest.java
+++ chess/src/test/java/ru/job4j/chess/firuges/black/BishopBlackTest.java
@@ -1,26 +1,32 @@
 package ru.job4j.chess.firuges.black;
 
-import junit.framework.TestCase;
+import org.junit.Test;
 import ru.job4j.chess.firuges.Cell;
 import ru.job4j.chess.firuges.Figure;
+
 import java.util.Arrays;
 
-public class BishopBlackTest extends TestCase {
+import static org.junit.Assert.*;
 
+public class BishopBlackTest {
+    @Test
     public void testPosition() {
         BishopBlack b = new BishopBlack(Cell.A8);
-        assertSame(b.position(),Cell.A8);
+        assertSame(b.position(), Cell.A8);
     }
 
+    @Test
     public void testWay() {
         BishopBlack bishopBlack = new BishopBlack(Cell.C1);
         Cell[] steps = {Cell.D2, Cell.E3, Cell.F4, Cell.G5};
         assertEquals(Arrays.asList(steps), Arrays.asList(bishopBlack.way(Cell.G5)));
     }
 
+    @Test
     public void testCopy() {
         BishopBlack b = new BishopBlack(Cell.A7);
         Figure f = b.copy(Cell.A7);
-        assertSame(b.position(),f.position());
+        assertSame(b.position(), f.position());
     }
+
 }
